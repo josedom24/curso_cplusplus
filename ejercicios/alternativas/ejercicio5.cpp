52c03ef31465d26dd15a85ba098b2a3bb9955eaf
José Domingo Muñoz <josedom24@gmail.com>
2019-06-24 21:13:23 +0200

    alternativas

---
+++ ejercicios/alternativas/ejercicio5.cpp
@@ -0,0 +1,41 @@
+//################################################################################
+//Escribe un programa que pida un nombre de usuario y una contraseña 
+//y si se ha introducido "pepe" y "asdasd" se indica "Has entrado al sistema", 
+//sino se da un error.
+//################################################################################
+// Análisis
+// Pedimos por teclado nombre y contraseña
+// si coinciden con las correctas indicamos que se ha entrado en el sistema.
+// en caso contrario indicamos que el nombre o la clave son incorrectas
+// Datos de entrada: usuario, password (cadena)
+// Información de salida: Mensajes de acierto o error.
+// Variables: usuario, password (cadena)
+//################################################################################
+// Diseño
+// 1. Leer el usuario
+// 2. Leer el password
+// 3. Si el usuario es igual a "pepe" y el password es igual a "asdasd" 
+//	  escribir "Has entrado en el sistema"
+// 4. En caso contrario mostrar el mensaje "Usuario/password incorrecto"
+//################################################################################
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string usuario, password;
+    cout << "Introduce el usuario: " << endl;
+    cin >> usuario;
+    cout << "Introduce el password: " << endl;
+    cin >> password;
+    if (usuario=="pepe" && password == "asdasd")
+    {
+        cout << "Has entrado al sistema";
+    }
+    else
+    {
+		cout << "Usuario/password incorrecto";
+    }
+    return 0;
+}
