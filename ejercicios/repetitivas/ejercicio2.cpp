fe10b58557dcf30809abdea642794d7b03554ac0
José Domingo Muñoz <josedom24@gmail.com>
2019-07-07 19:06:45 +0200

    ejercicio2

---
+++ ejercicios/repetitivas/ejercicio2.cpp
@@ -0,0 +1,72 @@
+//################################################################################
+// Crea una aplicación que permita adivinar un número. La aplicación genera un 
+// número aleatorio del 1 al 100. A continuación va pidiendo números y va 
+// respondiendo si el número a adivinar es mayor o menor que el introducido,
+// a demás de los intentos que te quedan (tienes 10 intentos para acertarlo). 
+// El programa termina cuando se acierta el número (además te dice en cuantos 
+//intentos lo has acertado), si se llega al limite de intentos te muestra el 
+//número que había generado.
+//################################################################################
+//Análisis
+//Tenemos que generar un número aleatorio (función Aleatorio) del 1 al 100.
+//Necesitamos un contador para contar los 10 intentos. Al principio vale 10 y 
+// irá decrementando.
+// Mientras no acierte el número y me queden intentos:
+// Leo un número
+// Lo comparo con el número generado (digo si es mayor o menor)
+// Pido otro numero
+// Puedo terminar el bucle por dos razones: si he adivinado el numero (he ganado)
+// y muestro los intentos que he necesitado.
+// sino he perdido, he utilizado los 10 intentos y muestro el número generado.
+// Datos de entrada: número pedido al usuario.
+// Información de salida: Has ganado y los intentos, o has perdido y el número. 
+// Variables: num_screto, num_ingresado, intentos (entero)
+//################################################################################
+//Diseño
+// 1.- Genero un numero aleatorio del 1 al 100 (num_secreto)
+// 2.- intentos = 10
+// 3.- Leer num
+// 4.- Mientras num<> num_secreto y intentos>1
+// 		5.- Si num_screto>num -> Escribir "Muy bajo"
+// 		6.- Si num_screto<num -> Escribir "Muy alto"
+//		7.- intentos=intentos-1
+//		8.- Escribir intentos
+//		9.- Leer num
+// 10.- Si num_secreo=num -> Escribir "Has ganado", intentos
+// 11.- SiNo -> Escribir "Has perdido, num_secreto
+
+//################################################################################
+#include <iostream>
+#include <stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int intentos,num_secreto,num_ingresado;
+	srand(time(NULL));
+    intentos = 10;
+    num_secreto  =  rand() % 100 + 1;
+    cout << "Adivine el numero (de 1 a 100):";
+    cin >> num_ingresado;
+    while (num_secreto!=num_ingresado && intentos>1) {
+        if(num_secreto>num_ingresado) {
+            cout << "Muy bajo" << endl;
+        }
+        else
+        {
+            cout << "Muy alto" << endl;
+        }
+        intentos  =  intentos-1;
+        cout << "Le quedan " << intentos << " intentos:";
+        cin >> num_ingresado;
+    }
+    
+    if(num_secreto==num_ingresado) {
+        cout << "Exacto! Usted adivino en " << 11-intentos << " intentos.";
+    }
+    else
+    {
+        cout << "El numero era: " << num_secreto;
+    }
+}
