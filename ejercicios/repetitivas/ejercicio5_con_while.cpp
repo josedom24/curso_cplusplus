85be0d38ab788950283fcd2a08db2a7b9e1424e5
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-09-09 17:23:01 +0200

    ejercicio5

--- ejercicios/repetitivas/ejercicio5_con_while.cpp
+++ ejercicios/repetitivas/ejercicio5_con_while.cpp
@@ -1,19 +1,19 @@
 //################################################################################
 //Algoritmo que pida caracteres e imprima 'VOCAL' si son vocales y 'NO VOCAL' 
-//en caso contrario, el programa termina cuando se introduce un espacio.
+//en caso contrario, el programa termina cuando se introduce un asterisco.
 //################################################################################
 //Análisis
-//Leer un carácter hasta que sea el espacio.
-//Si el carácter no es espacio -> si es una vocal -> Muestro "Es vocal"
+//Leer un carácter hasta que sea el asterisco.
+//Si el carácter no es asterisco -> si es una vocal -> Muestro "Es vocal"
 //Si no muestro "No es vocal"
 // Datos de entrada:vamos leyendo un carácter.
 // Información de salida:Mensajes: "Es vocal", "No es vocal"
-// Variables:car (caracter)
+// Variables:car (carácter)
 //################################################################################
 //Diseño
 //1.- Repetir -> Leer carácter
-//2.- Mientras no sea espacio
-//		3.- Si car no es el espacio
+//2.- Mientras no sea asterisco
+//		3.- Si car no es el asterisco
 //		4.- Si es A,E,I,O,U o a,e,i,o,u -> Mostrar "Es vocal"
 //		5.-Si no mostrar "No es vocal"
 //		6.- Repetir -> Leer carácter
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 		cin >> car;
 	}
 	while(car.size()!=1);
-	while(car!= " ") {
+	while(car!= "*") {
 		
 		cout << "Introduce un carácter:";
 		cin >> car;
