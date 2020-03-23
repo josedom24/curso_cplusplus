eab3aed36dd3623cc7ddd892e456ecc64b5bbb65
José Domingo Muñoz <josedom24@gmail.com>
2020-03-23 12:21:26 +0100

    ejercicio14

---
+++ ejercicios/repetitivas/ejercicio14.cpp
@@ -0,0 +1,39 @@
+//################################################################################
+//Una persona se encuentra en el kilómetro 70 de una carretera, otra se encuentra 
+//en el km 150, los coches tienen sentido opuesto y tienen la misma velocidad. 
+//Realizar un programa para determinar en qué kilómetro de esa carretera se 
+//encontrarán.
+//################################################################################
+//Análisis
+//tengo que calcular en que km se van a encontrar los coches. sabemos que van en
+//sentido opuesto por lo tanto, si llevan la misma velocidad, el primero va a ir
+//incrementando el km donde se encuentra y el segundo va a ir decrementándolo.
+//Cuando coincidan en el mismo km, podemos considerar que se han encontrado.
+// Datos de entrada: Nada, vamos a inicializar las variables donde guardamos los km 
+// Información de salida: El km donde se encuentran.
+// Variables: km1,km2 (enteros)
+//################################################################################
+//Diseño
+//1.- Inicializo las variables kms
+//2.- Mientras km1<>km2
+//		3.- Incremento km1
+//		4.- Incremento km2
+//5.- Mostrar en el km que se encuentran
+//################################################################################
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int  km1,km2;
+	km1=70;
+	km2=150;
+	while(km1!=km2)
+	{
+		km1++;
+		km2--;
+	}
+	
+	cout << "Se encuentran en el km:" << km1;
+	return 0;
+}
