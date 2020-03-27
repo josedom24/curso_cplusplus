c91d397ea3e0f01a118f0c6b8774273824b9ca69
José Domingo Muñoz <josedom24@gmail.com>
2020-03-27 14:54:25 +0100

    ejercicio3

--- ejercicios/cadenas/ejercicio3_con_recorrido.cpp
+++ ejercicios/cadenas/ejercicio3_con_recorrido.cpp
@@ -3,8 +3,7 @@
 //y muestra cuantas veces aparece el carácter en la cadena.
 //################################################################################
 //Análisis
-//Leo una cadena y un carácter (tengo que asegurarme que es un sólo carácter,
-//es decir hasta que la longitud sea 1). Recorro la cadena y compara cada uno de
+//Leo una cadena y un carácter. Recorro la cadena y compara cada uno de
 //sus caracteres con el carácter introducido, si es igual lo cuento.
 // Datos de entrada: Cadena y carácter
 // Información de salida: Número de veces que aparece el carácter en la cadena.
