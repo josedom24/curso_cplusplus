21dc411a7e8501a8fcc148d98dc975dcfc673904
José Domingo Muñoz <josedom24@gmail.com>
2020-04-06 12:57:43 +0200

    ejercicio6

---
+++ ejercicios/cadenas/ejercicio6.cpp
@@ -0,0 +1,29 @@
+//################################################################################
+//Realizar un programa que dada una cadena de caracteres por caracteres, genere 
+//otra cadena resultado de invertir la primera.
+//################################################################################
+//Análisis
+//Leemos una cadena, la recorremos desde el final al principio y cada carácter 
+//lo vamos concatenando con una nueva cadena, que inicialmente tendrá como valor
+//la cadena vacía­.
+// Datos de entrada: Una cadena de caracteres.
+// Información de salida: La cadena invertida.
+// Variables: cad,invertida (Caracter)
+//################################################################################
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string cad,invertida="";
+	int posicion;
+	
+	cout << "Introduce una cadena:";
+	getline(cin,cad);
+
+	for(posicion=cad.length()-1;posicion>=0;posicion--)
+		invertida += cad[posicion];
+	
+	cout<< "La cadena invertida es:"<< invertida;
+	return 0;
+}
